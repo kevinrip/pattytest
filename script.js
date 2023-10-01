fbf14ede1c2f8f5ae3d3064bbedc74fcbb2376c9
kevinrip <48979362+kevinrip@users.noreply.github.com>
2023-09-30 22:15:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let currentIndex = 0; // Current index of the displayed item
 
 // Function to fetch data from the API endpoint
 async function fetchData() {
@@ -8,20 +9,42 @@ async function fetchData() {
         const dataDiv = document.getElementById('data');
         dataDiv.innerHTML = ''; // Clear previous data
         
-        // Loop through the data and create HTML elements to display it
-        data.forEach(item => {
-            const itemDiv = document.createElement('div');
-            itemDiv.innerHTML = `
-                <p>Name: ${item['Leave your name if you\'d like:']}</p>
-                <p>Memories: ${item['Share any memories, messages, stories here:']}</p>
-                <p>Word: ${item['What\'s one word, feeling, expression, etc'][' that reminds you of Patty?']}</p>
-                <hr>
-            `;
-            dataDiv.appendChild(itemDiv);
-        });
+        // Create HTML elements to display the current item
+        const currentItem = data[currentIndex];
+        const itemDiv = document.createElement('div');
+        itemDiv.innerHTML = `
+            <p>Name: ${currentItem['Leave your name if you\'d like:']}</p>
+            <p>Memories: ${currentItem['Share any memories, messages, stories here:']}</p>
+            <p>Word: ${currentItem['What\'s one word, feeling, expression, etc'][' that reminds you of Patty?']}</p>
+            <hr>
+        `;
+        dataDiv.appendChild(itemDiv);
+
+        // Create navigation buttons
+        const prevButton = document.createElement('button');
+        prevButton.textContent = 'Previous';
+        prevButton.addEventListener('click', showPreviousItem);
+        dataDiv.appendChild(prevButton);
+
+        const nextButton = document.createElement('button');
+        nextButton.textContent = 'Next';
+        nextButton.addEventListener('click', showNextItem);
+        dataDiv.appendChild(nextButton);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
+}
+
+// Function to show the previous item
+function showPreviousItem() {
+    currentIndex = (currentIndex - 1 + data.length) % data.length;
+    fetchData();
+}
+
+// Function to show the next item
+function showNextItem() {
+    currentIndex = (currentIndex + 1) % data.length;
+    fetchData();
 }
 
 // Call the fetchData function when the window is loaded
